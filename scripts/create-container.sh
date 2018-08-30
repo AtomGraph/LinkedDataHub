2fb058227adb30cb497424ab68fb8b75094a7352
Martynas <martynas@atomgraph.com>
2018-08-31 00:44:34 +0200

    New version of scripts that accept named arguments, e.g.: ./create-container.sh -f ../../linkeddatahub-apps/certs/martynas.stage.localhost.pem -p XXXXXX -b https://localhost:4443/demo/city-graph/  https://localhost:4443/demo/city-graph/ --title "Test" --description "This is a container"

--- scripts/create-container.sh
+++ scripts/create-container.sh
@@ -1,20 +1,64 @@
 #!/bin/bash
 
-[ -z "$JENAROOT" ] && echo "Need to set JENAROOT" && exit 1;
+# New version of scripts that accept named arguments, e.g.: ./create-container.sh -f ../../linkeddatahub-apps/certs/martynas.stage.localhost.pem -p XXXXXX -b https://localhost:4443/demo/city-graph/  https://localhost:4443/demo/city-graph/ --title "Test" --description "This is a container"
 
-if [ "$#" -ne 6 ]; then
-  echo "Usage:   $0 base parent cert_pem_file cert_password title slug" >&2
-  echo "Example: $0" 'https://linkeddatahub.com/my-context/my-dataspace/ https://linkeddatahub.com/my-context/my-dataspace/ linkeddatahub.pem Password "My container" my-container' >&2
-  exit 1
+args=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -b|--base)
+    base="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --title)
+    title="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --description)
+    description="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --slug)
+    slug="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown arguments
+    args+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${args[@]}" # restore args parameters
+
+if [ -z "$base" ] ; then
+    echo '-b|--base not set'
+    exit 1
+fi
+if [ -z "$title" ] ; then
+    echo '--title not set'
+    exit 1
+fi
+args+=("-c")
+args+=("${base}ns/default#Container")
+args+=("-t")
+args+=("text/turtle")
+
+turtle+="@prefix def:	<ns/default#> .\n"
+turtle+="@prefix dct:	<http://purl.org/dc/terms/> .\n"
+turtle+="@prefix dh:	<https://www.w3.org/ns/ldt/document-hierarchy/domain#> .\n"
+turtle+="_:container a def:Container .\n"
+turtle+="_:container dct:title \"${title}\" .\n"
+if [ ! -z "$description" ] ; then
+    turtle+="_:container dct:description \"${description}\" .\n"
+fi
+if [ ! -z "$slug" ] ; then
+    turtle+="_:container dh:slug \"${slug}\" .\n"
 fi
 
-base=$1
-parent=$2
-cert_pem_file=$3
-cert_password=$4
-class=${base}ns/default#Container
-
-export title=$5
-export slug=$6
-
-envsubst < container.ttl | turtle --base=${base} | ./create-document.sh "$parent" "$cert_pem_file" "$cert_password" "application/n-triples" "$class"
+echo -e $turtle | turtle --base="${base}" | ./create-document.sh "${args[@]}"
--- scripts/create-document.sh
+++ scripts/create-document.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
 
-[ -z "$JENAROOT" ] && echo "Need to set JENAROOT" && exit 1;
+# ./create-document.sh -f shit -t text/turtle -c Class http://target -p XXX
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage:   cat doc.ttl | $0 container cert_pem_file cert_password content_type class" >&2
-  echo "Example: cat doc.ttl | $0" 'https://linkeddatahub.com/my-context/my-dataspace/ linkeddatahub.pem Password application/n-triples "https://linkeddatahub.com/my-context/my-dataspace/ns#Class"' >&2
-  exit 1
+UNKNOWN=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -f|--cert-pem-file)
+    cert_pem_file="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--cert-password)
+    cert_password="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -c|--class)
+    class="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--content-type)
+    content_type="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    UNKNOWN+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${UNKNOWN[@]}" # restore unknown parameters
+
+if [ -z "$cert_pem_file" ] ; then
+    echo '-f|--cert_pem_file not set'
+    exit 1
+fi
+
+if [ -z "$cert_password" ] ; then
+    echo '-p|--cert-password not set'
+    exit 1
+fi
+
+if [ -z "$class" ] ; then
+    echo '-c|--class not set'
+    exit 1
+fi
+
+if [ -z "$content_type" ] ; then
+    echo '-t|--content-type not set'
+    exit 1
 fi
 
 urlencode()
@@ -13,13 +61,19 @@ urlencode()
   echo $(python -c 'import urllib, sys; print urllib.quote(sys.argv[1] if len(sys.argv) > 1 else sys.stdin.read()[0:-1])' $1)
 }
 
+echo "class        = ${class}"
+echo "content_type = ${content_type}"
+
+if [ "$#" -ne 1 ]; then
+    echo "Only one default argument is allowed"
+    exit 1
+fi
+
 container=$1
-cert_pem_file=$2
-cert_password=$3
-content_type=$4
-class=$5
+echo "container    = ${container}"
 
 target=${container}?forClass=$(urlencode "$class")
+echo "target       = ${target}"
 
 # POST RDF document from stdin to the server and print Location URL
 
