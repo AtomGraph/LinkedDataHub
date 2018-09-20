10f1a0b8f63f2b0e7bea966c3b8d7a29800d165e
Martynas <martynas@atomgraph.com>
2018-09-21 01:15:45 +0200

    clear-ontology refactored

--- scripts/admin/clear-ontology.sh
+++ scripts/admin/clear-ontology.sh
@@ -2,14 +2,47 @@
 
 [ -z "$JENAROOT" ] && echo "Need to set JENAROOT" && exit 1;
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage:   $0 cert_pem_file cert_password ontology_doc" >&2
-  echo "Example: $0" 'martynas.linkeddatahub.pem Password https://linkeddatahub.com/atomgraph/city-graph/admin/sitemap/ontologies/templates' >&2
-  exit 1
+args=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -f|--cert-pem-file)
+    cert_pem_file="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--cert-password)
+    cert_password="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown arguments
+    args+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${args[@]}" # restore args
+
+if [ -z "$cert_pem_file" ] ; then
+    # echo '-f|--cert_pem_file not set'
+    print_usage
+    exit 1
 fi
 
-cert_pem_file=$1
-cert_password=$2
-ontology_doc=$3
+if [ -z "$cert_password" ] ; then
+    # echo '-p|--cert-password not set'
+    print_usage
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "Only one default argument is allowed"
+    exit 1
+fi
+
+ontology_doc=$1
 
 curl -v -k -E ${cert_pem_file}:${cert_password} "${ontology_doc}?clear=" -H "Accept: text/turtle"
