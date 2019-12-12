c6a198fef08e01ef9092799c34b783b97f28b5f1
Martynas <martynas@atomgraph.com>
2019-12-13 00:01:58 +0100

    update-document CLI script

--- scripts/update-document.sh
+++ scripts/update-document.sh
@@ -9,7 +9,6 @@ print_usage()
     printf "Options:\n"
     printf "  -f, --cert-pem-file CERTIFICATE      .pem file with the WebID certificate of the agent\n"
     printf "  -p, --cert-password CERT_PASSWORD    Password of the WebID certificate (provided during signup)\n"
-    printf "  -c, --class CLASS_URI                URI of the class that is the type of the document being created\n"
     printf "  -t, --content-type MEDIA_TYPE        Media type of the RDF body (e.g. text/turtle)\n"
 }
 
@@ -28,11 +27,6 @@ case $key in
     ;;
     -p|--cert-password)
     cert_password="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -c|--class)
-    class="$2"
     shift # past argument
     shift # past value
     ;;
@@ -61,31 +55,18 @@ if [ -z "$cert_password" ] ; then
     exit 1
 fi
 
-if [ -z "$class" ] ; then
-    # echo '-c|--class not set'
-    print_usage
-    exit 1
-fi
-
 if [ -z "$content_type" ] ; then
     # echo '-t|--content-type not set'
     print_usage
     exit 1
 fi
 
-urlencode()
-{
-    python -c 'import urllib, sys; print urllib.quote(sys.argv[1] if len(sys.argv) > 1 else sys.stdin.read()[0:-1])' "$1"
-}
-
 if [ "$#" -ne 1 ]; then
     echo "Only one default argument is allowed"
     exit 1
 fi
 
-container="$1"
-forClass=$(urlencode "$class")
-target="${container}?forClass=${forClass}"
+target="$1"
 
-# POST RDF document from stdin to the server and print Location URL
-cat - | curl -v -k -E "${cert_pem_file}":"${cert_password}" -d @- -H "Content-Type: ${content_type}" -H "Accept: text/turtle" "${target}" -s -D - | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p'
+# PUT RDF document from stdin to the server and print Location URL
+cat - | curl -v -k -E "${cert_pem_file}":"${cert_password}" -X PUT -d @- -H "Content-Type: ${content_type}" -H "Accept: text/turtle" "${target}" -s -D - | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p'
