47acdad95942ff0a13dd2117f101b6a9b7182a27
Martynas <martynas@atomgraph.com>
2021-01-25 10:24:58 +0100

    Additional HTTP smoke tests

---
+++ http-tests/linked-data-templates/GET.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+
+pushd . > /dev/null && cd "$SCRIPT_ROOT/admin/acl"
+
+# add agent to the writers group to be able to read/write documents (might already be done by another test)
+
+./add-agent-to-group.sh \
+  -f "$OWNER_CERT_FILE" \
+  -p "$OWNER_CERT_PWD" \
+  --agent "$AGENT_URI" \
+  "${ADMIN_BASE_URL}acl/groups/readers/"
+
+popd > /dev/null
+
+# check that HTML response works (use Chrome's default Accept value)
+
+curl --head -k -w "%{http_code}\n" -f -s \
+  -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  "${END_USER_BASE_URL}" \
+| grep -q "${STATUS_OK}"
