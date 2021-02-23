c842f5c86ef14cf95b36e9cfbb3b4f92de3e2a14
Martynas <martynas@atomgraph.com>
2021-02-23 21:05:12 +0100

    Try to replace $request_base with $base using sed

--- scripts/admin/add-ontology-import.sh
+++ scripts/admin/add-ontology-import.sh
@@ -75,7 +75,7 @@ echo "ontology_doc: $ontology_doc"
 if [ -z "$request_base" ] ; then
     ontology_doc_uri="$ontology_doc"
 else
-    ontology_doc_uri="${$ontology_doc/$request_base/$base}"
+    ontology_doc_uri=$(echo "$ontology_doc" | sed -i "s|$request_base|$base|g")
 fi
 
 echo "ontology_doc_uri: $ontology_doc_uri"
