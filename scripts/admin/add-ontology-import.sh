223836414f979b4ceabc1d1d56b418c93a3ab274
Martynas <martynas@atomgraph.com>
2021-02-23 21:12:21 +0100

    Show curl response

--- scripts/admin/add-ontology-import.sh
+++ scripts/admin/add-ontology-import.sh
@@ -82,7 +82,7 @@ echo "ontology_doc_uri: $ontology_doc_uri"
 
 # extract ontology URI and graph URI from app document N-Triples description (slashes in ${ontology_doc} need to be escaped before passing to sed)
 
-curl -v -k -E "$cert_pem_file":"$cert_password" "$ontology_doc" -H "Accept: application/n-triples" | cat | sed -rn "s/<${ontology_doc_uri//\//\\/}> <http:\/\/xmlns.com\/foaf\/0.1\/primaryTopic> <(.*)> \./\1/p"
+curl -v -k -E "$cert_pem_file":"$cert_password" "$ontology_doc" -H "Accept: application/n-triples"
 
 ontology=$(curl -s -k -E "$cert_pem_file":"$cert_password" "$ontology_doc" -H "Accept: application/n-triples" | cat | sed -rn "s/<${ontology_doc_uri//\//\\/}> <http:\/\/xmlns.com\/foaf\/0.1\/primaryTopic> <(.*)> \./\1/p")
 echo "ontology: $ontology"
