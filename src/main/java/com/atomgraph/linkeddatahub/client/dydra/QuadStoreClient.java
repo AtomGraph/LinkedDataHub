5c4c577ee34e8f9f8921ab02fdbff59482765f59
Martynas <martynas@atomgraph.com>
2021-03-09 11:54:31 +0100

    SNAPSHOT bump Added missing license headers Added interfaces Async as well as DatasetQuadAccessorAsync

--- src/main/java/com/atomgraph/linkeddatahub/client/dydra/QuadStoreClient.java
+++ src/main/java/com/atomgraph/linkeddatahub/client/dydra/QuadStoreClient.java
@@ -1,3 +1,19 @@
+/**
+ *  Copyright 2020 Martynas Jusevičius <martynas@atomgraph.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.atomgraph.linkeddatahub.client.dydra;
 
 import com.atomgraph.core.MediaTypes;
@@ -12,7 +28,7 @@ import org.apache.jena.query.Dataset;
  * @author Martynas Jusevičius {@literal <martynas@atomgraph.com>}
  * @see <a href="https://api.dydra.com/graphstore/asynchronous.html">Asynchronous Processing</a>
  */
-public class QuadStoreClient extends com.atomgraph.core.client.QuadStoreClient implements DatasetAccessorAsync
+public class QuadStoreClient extends com.atomgraph.core.client.QuadStoreClient implements DatasetQuadAccessorAsync
 {
 
     public QuadStoreClient(WebTarget endpoint, MediaTypes mediaTypes)
@@ -34,13 +50,8 @@ public class QuadStoreClient extends com.atomgraph.core.client.QuadStoreClient i
     {
         return new QuadStoreClient(endpoint);
     }
-    
-//    @Override
-//    public void add(Dataset dataset)
-//    {
-//        add(dataset, DatasetAccessorAsync.Mode.NOTIFY);
-//    }
-    ;
+
+    @Override
     public void add(Dataset dataset, DatasetAccessorAsync.Mode mode)
     {
         MultivaluedHashMap headers = new MultivaluedHashMap();
@@ -49,12 +60,7 @@ public class QuadStoreClient extends com.atomgraph.core.client.QuadStoreClient i
         post(dataset, getDefaultMediaType(), new MediaType[]{}, new MultivaluedHashMap(), headers).close();
     }
 
-//    @Override
-//    public void replace(Dataset dataset)
-//    {
-//        replace(dataset, DatasetAccessorAsync.Mode.NOTIFY);
-//    }
-
+    @Override
     public void replace(Dataset dataset, DatasetAccessorAsync.Mode mode)
     {
         MultivaluedHashMap headers = new MultivaluedHashMap();
