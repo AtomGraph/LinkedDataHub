63a126aeca07dce682b0cbc5cc598d9eaae37884
Martynas <martynas@atomgraph.com>
2021-03-20 22:39:13 +0100

    Fixed bash syntax

--- http-tests/run.sh
+++ http-tests/run.sh
@@ -80,7 +80,7 @@ function restart_backend_cache()
 
     echo "RESTART $service_name !!!"
 
-    if [ -z "$(docker-compose ps -q | grep "$( docker-compose ps -q $service_name )" )" ]
+    if [ -z "$(docker-compose ps -q | grep "$( docker-compose ps -q $service_name )" )" ]; then
         docker-compose restart "$1"
     fi
 }
