d884a07d42fa50c339ae609feb62b7e0f124d336
Martynas <martynas@atomgraph.com>
2021-03-25 09:47:10 +0100

    purge_backend_cache before all HTTP test scripts

--- http-tests/request-access.sh
+++ http-tests/request-access.sh
@@ -2,6 +2,8 @@
 
 initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
 initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_backend_cache "$END_USER_VARNISH_SERVICE"
+purge_backend_cache "$ADMIN_VARNISH_SERVICE"
 
 curl -w "%{http_code}\n" -k -s \
   -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
