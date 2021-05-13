88bb3be0890e0dcbd850924a177201d8d45594b4
Martynas <martynas@atomgraph.com>
2021-05-13 17:38:43 +0200

    Debug curl call

--- platform/entrypoint.sh
+++ platform/entrypoint.sh
@@ -225,12 +225,11 @@ wait_for_url()
     local counter="$4"
     local accept="$5"
     i=1
-echo "WTF counter: $counter"
+
     # use HTTP Basic auth if username/password are provided
     if [ -n "$auth_user" ] && [ -n "$auth_pwd" ] ; then
         while [ "$i" -le "$counter" ] && ! curl -s -f -X OPTIONS "$url" --user "$auth_user":"$auth_pwd" -H "Accept: ${accept}" >/dev/null 2>&1
         do
-echo "WTF i: $i"
             sleep 1 ;
             i=$(( i+1 ))
         done
@@ -512,6 +511,9 @@ if [ "$LOAD_DATASETS" = "true" ]; then
 
     trig --base="$root_admin_base_uri" "$ADMIN_DATASET" > /var/linkeddatahub/based-datasets/admin.nq
     sparql --data /var/linkeddatahub/based-datasets/admin.nq --base "$root_admin_base_uri" --query split-default-graph.rq --results=nq > /var/linkeddatahub/based-datasets/split.admin.nq
+
+    echo "WTF"
+    curl "$root_end_user_quad_store_url"
 
     printf "\n### Waiting for %s... TIMEOUT: %s\n" "$root_end_user_quad_store_url" "$TIMEOUT"
     wait_for_url "$root_end_user_quad_store_url" "$root_end_user_service_auth_user" "$root_end_user_service_auth_pwd" "$TIMEOUT" "application/n-quads"
