06850700f51850e1103b646bbcbcab387a20eae8
Martynas <martynas@atomgraph.com>
2021-05-21 14:30:09 +0200

    Removed $graph_doc indirection in add-agent-to-group.sh -- using $group_doc as the group's named graph URI

--- scripts/admin/acl/add-agent-to-group.sh
+++ scripts/admin/acl/add-agent-to-group.sh
@@ -59,15 +59,13 @@ group_doc="$1"
 
 group=$(curl -s -k -E "$cert_pem_file":"$cert_password" "$group_doc" -H "Accept: application/n-triples" | cat | sed -rn "s/<${group_doc//\//\\/}> <http:\/\/xmlns.com\/foaf\/0.1\/primaryTopic> <(.*)> \./\1/p")
 
-graph_doc=$(curl -s -k -E "$cert_pem_file":"$cert_password" "$group_doc" -H "Accept: application/n-triples" | cat | sed -rn "s/<${group_doc//\//\\/}> <http:\/\/rdfs\.org\/ns\/void#inDataset> <(.*)#this> \./\1/p")
-
 sparql+="PREFIX foaf:	<http://xmlns.com/foaf/0.1/>\n"
 sparql+="INSERT DATA {\n"
-sparql+="  GRAPH <${graph_doc}> {\n"
+sparql+="  GRAPH <${group_doc}> {\n"
 sparql+="    <${group}> foaf:member <${agent}> .\n"
 sparql+="  }\n"
 sparql+="}\n"
 
 # PATCH SPARQL to the named graph
 
-echo -e "$sparql" | curl -X PATCH --data-binary @- -s -k -E "$cert_pem_file":"$cert_password" "$graph_doc" -H "Content-Type: application/sparql-update"
+echo -e "$sparql" | curl -X PATCH --data-binary @- -s -k -E "$cert_pem_file":"$cert_password" "$group_doc" -H "Content-Type: application/sparql-update"
