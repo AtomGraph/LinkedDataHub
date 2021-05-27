6e4b14ff96e014e31b55be6cb97eca466df74573
Martynas <martynas@atomgraph.com>
2021-05-27 13:29:46 +0200

    Actually include forClass param in target URL

--- scripts/create-document.sh
+++ scripts/create-document.sh
@@ -79,4 +79,4 @@ else
 fi
 
 # POST RDF document from stdin to the server and print Location URL
-cat - | curl -v -k -E "${cert_pem_file}":"${cert_password}" -d @- -H "Content-Type: ${content_type}" -H "Accept: text/turtle" "$1" -v -D - | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p'
+cat - | curl -v -k -E "${cert_pem_file}":"${cert_password}" -d @- -H "Content-Type: ${content_type}" -H "Accept: text/turtle" "$target" -v -D - | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p'
