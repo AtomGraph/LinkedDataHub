7175b55e8ad1cb63dd9ab3e16bd3a5713d97d3e3
Martynas <martynas@atomgraph.com>
2021-06-11 13:29:19 +0200

    Actually commit Item class :)

---
+++ src/main/java/com/atomgraph/linkeddatahub/resource/graph/Item.java
@@ -0,0 +1,80 @@
+/**
+ *  Copyright 2121 Martynas Jusevičius <martynas@atomgraph.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.atomgraph.linkeddatahub.resource.graph;
+
+import com.atomgraph.core.MediaTypes;
+import com.atomgraph.linkeddatahub.model.Service;
+import com.atomgraph.linkeddatahub.server.model.impl.GraphStoreImpl;
+import java.net.URI;
+import java.util.Optional;
+import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Providers;
+import org.apache.jena.rdf.model.Model;
+
+/**
+ *
+ * @author {@literal Martynas Jusevičius <martynas@atomgraph.com>}
+ */
+public class Item extends GraphStoreImpl
+{
+
+    private final URI uri;
+    
+    @Inject
+    public Item(@Context Request request, Optional<Service> service, MediaTypes mediaTypes,
+        @Context UriInfo uriInfo, @Context Providers providers, com.atomgraph.linkeddatahub.Application system)
+    {
+        super(request, service, mediaTypes, uriInfo, providers, system);
+        this.uri = uriInfo.getAbsolutePath();
+    }
+
+    @Override
+    public Response get(@QueryParam("default") @DefaultValue("false") Boolean defaultGraph, @QueryParam("graph") URI graphUri)
+    {
+        return super.get(false, getURI());
+    }
+    
+    @Override
+    public Response post(Model model, @QueryParam("default") @DefaultValue("false") Boolean defaultGraph, @QueryParam("graph") URI graphUri)
+    {
+        return super.post(model, false, getURI());
+    }
+    
+    @Override
+    public Response put(Model model, @QueryParam("default") @DefaultValue("false") Boolean defaultGraph, @QueryParam("graph") URI graphUri)
+    {
+        return super.put(model, false, getURI());
+    }
+    
+    @Override
+    public Response delete(@QueryParam("default") @DefaultValue("false") Boolean defaultGraph, @QueryParam("graph") URI graphUri)
+    {
+        return super.delete(false, getURI());
+    }
+    
+    public URI getURI()
+    {
+        return uri;
+    }
+    
+}
