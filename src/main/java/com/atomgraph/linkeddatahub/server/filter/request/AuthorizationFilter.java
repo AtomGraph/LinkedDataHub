0cf420e3d9e9f80afbcfc4da368cbef4b8be86ea
Martynas <martynas@atomgraph.com>
2021-08-27 13:27:13 +0200

    Check for "primitive apps" only when getApplication().isPresent()

--- src/main/java/com/atomgraph/linkeddatahub/server/filter/request/AuthorizationFilter.java
+++ src/main/java/com/atomgraph/linkeddatahub/server/filter/request/AuthorizationFilter.java
@@ -97,8 +97,7 @@ public class AuthorizationFilter implements ContainerRequestFilter
         if (request == null) throw new IllegalArgumentException("ContainerRequestContext cannot be null");
         if (log.isDebugEnabled()) log.debug("Authorizing request URI: {}", request.getUriInfo().getRequestUri());
 
-//        if (getApplication().isEmpty()) return; // skip filter if no application has matched
-        if (!getApplication().get().canAs(EndUserApplication.class) && !getApplication().get().canAs(AdminApplication.class)) return; // skip "primitive" apps
+        if (getApplication().isPresent() && (!getApplication().get().canAs(EndUserApplication.class) && !getApplication().get().canAs(AdminApplication.class))) return; // skip "primitive" apps
 
         Resource accessMode = ACCESS_MODES.get(request.getMethod());
         if (log.isDebugEnabled()) log.debug("Request method: {} ACL access mode: {}", request.getMethod(), accessMode);
