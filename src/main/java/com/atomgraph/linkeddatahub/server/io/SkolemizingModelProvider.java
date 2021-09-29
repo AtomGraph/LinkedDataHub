bf2c9dba85870ebe33f3166bfcb3a6f0d25a7960
Martynas <martynas@atomgraph.com>
2021-09-30 00:16:14 +0200

    Removed restrictions from the adm: ontology All resources get dh:slug during POST/PUT ldt:fragment instead of ldt:path on query

--- src/main/java/com/atomgraph/linkeddatahub/server/io/SkolemizingModelProvider.java
+++ src/main/java/com/atomgraph/linkeddatahub/server/io/SkolemizingModelProvider.java
@@ -148,26 +148,28 @@ public class SkolemizingModelProvider extends com.atomgraph.server.io.Skolemizin
         super.process(resource);
         
         // append dh:slug property with UUID value to apl:Content instances and dh:Container/dh:Item subclass instances
-        if (getOntology().isPresent() && !resource.hasProperty(DH.slug))
-            {
-                Statement typeStmt = resource.getProperty(RDF.type);
-                if (typeStmt != null && typeStmt.getObject().isURIResource())
-                {
-                    OntClass ontClass = getOntology().get().getOntModel().getOntClass(typeStmt.getResource().getURI());
-                    if (ontClass != null)
-                    {
-                        if (ontClass.equals(APL.Content)) resource.addLiteral(DH.slug, UUID.randomUUID().toString());
-                        else
-                        {
-                            // cannot use ontClass.hasSuperClass() here as it does not traverse the chain
-                            Set<OntClass> superClasses = ontClass.listSuperClasses().toSet();
-                            if (superClasses.contains(DH.Container) || superClasses.contains(DH.Item))
-                                resource.addLiteral(DH.slug, UUID.randomUUID().toString());
-                        }
-                    }
-                }
-            }
+//        if (getOntology().isPresent() && !resource.hasProperty(DH.slug))
+//        {
+//            Statement typeStmt = resource.getProperty(RDF.type);
+//            if (typeStmt != null && typeStmt.getObject().isURIResource())
+//            {
+//                OntClass ontClass = getOntology().get().getOntModel().getOntClass(typeStmt.getResource().getURI());
+//                if (ontClass != null)
+//                {
+//                    if (ontClass.equals(APL.Content)) resource.addLiteral(DH.slug, UUID.randomUUID().toString());
+//                    else
+//                    {
+//                        // cannot use ontClass.hasSuperClass() here as it does not traverse the chain
+//                        Set<OntClass> superClasses = ontClass.listSuperClasses().toSet();
+//                        if (superClasses.contains(DH.Container) || superClasses.contains(DH.Item))
+//                            resource.addLiteral(DH.slug, UUID.randomUUID().toString());
+//                    }
+//                }
+//            }
+//        }
 
+        if (!resource.hasProperty(DH.slug)) resource.addLiteral(DH.slug, UUID.randomUUID().toString()); // all resources get slugs
+                
         if (resource.hasProperty(DCTerms.format) && resource.getProperty(DCTerms.format).getObject().isLiteral())
         {
             Resource format = resource.getProperty(DCTerms.format).
