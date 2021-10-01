7c50795971cc31a2eb5505cfd100c2ce3b4065b9
Martynas <martynas@atomgraph.com>
2021-10-01 13:12:41 +0200

    cd to find create-document.sh

--- scripts/admin/acl/create-authorization.sh
+++ scripts/admin/acl/create-authorization.sh
@@ -220,5 +220,11 @@ if [ -n "$write" ] ; then
     turtle+="${auth} acl:mode acl:Write .\n"
 fi
 
+pushd . > /dev/null && cd "$SCRIPT_ROOT"
+
 # submit Turtle doc to the server
-echo -e "$turtle" | turtle --base="$base" | ../../create-document.sh "${args[@]}"
+echo -e "$turtle" | turtle --base="$base" | ./create-document.sh "${args[@]}"
+
+popd > /dev/null
+
+echo "$item"
