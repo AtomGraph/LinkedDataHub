c42a8d453031822b1a2e1416072b8eb4951ebb13
Martynas <martynas@atomgraph.com>
2022-01-17 23:02:20 +0100

    Fixed BAN condition

--- src/main/java/com/atomgraph/linkeddatahub/resource/oauth2/Login.java
+++ src/main/java/com/atomgraph/linkeddatahub/resource/oauth2/Login.java
@@ -243,7 +243,7 @@ public class Login extends GraphStoreImpl
                         }
 
                         // purge agent lookup from proxy cache
-                        if (getApplication().getService().getProxy() == null) ban(getApplication().getService().getProxy(), jwt.getSubject());
+                        if (getApplication().getService().getProxy() != null) ban(getApplication().getService().getProxy(), jwt.getSubject());
                         
                         // remove secretary WebID from cache
                         getSystem().getEventBus().post(new com.atomgraph.linkeddatahub.server.event.SignUp(getSystem().getSecretaryWebIDURI()));
