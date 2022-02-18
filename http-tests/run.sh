e2d53c37f4285e95ce4ca650374548760f7cae76
Martynas <martynas@atomgraph.com>
2022-02-18 16:47:43 +0100

    Added GET-agent-mbox-ntriples HTTP test

--- http-tests/run.sh
+++ http-tests/run.sh
@@ -122,9 +122,9 @@ download_dataset "$ADMIN_ENDPOINT_URL" > "$TMP_ADMIN_DATASET"
 
 ### Other tests ###
 
-run_tests $(find . -type f -name 'request-access-html.sh')
+run_tests $(find . -type f -name 'GET-request-access-html.sh')
 (( error_count += $? ))
-run_tests $(find . -type f -name 'request-access.sh')
+run_tests $(find . -type f -name 'POST-request-access.sh')
 (( error_count += $? ))
 run_tests $(find . -type f -name 'webid-delegation.sh')
 (( error_count += $? ))
