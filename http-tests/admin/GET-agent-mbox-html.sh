96b9f35ba5894551854706d7c0610e54071c3854
Martynas <martynas@atomgraph.com>
2022-02-18 17:01:03 +0100

    Moved more tests to admin/ Added GET-agent-mbox-html test

---
+++ http-tests/admin/GET-agent-mbox-html.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_backend_cache "$END_USER_VARNISH_SERVICE"
+purge_backend_cache "$ADMIN_VARNISH_SERVICE"
+
+# request agent's HTML description
+
+ntriples=$(curl -k -f -s -N \
+    -H 'Accept: text/html' \
+  "$AGENT_URI")
+
+# check that the description does *not* include foaf:mbox property
+
+echo "$ntriples" | grep -q -v "http://xmlns.com/foaf/0.1/mbox"
+
+# check that the description does includes foaf:mbox_sha1sum property
+
+echo "$ntriples" | grep -q "http://xmlns.com/foaf/0.1/mbox_sha1sum"
