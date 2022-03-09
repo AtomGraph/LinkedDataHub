4b3770796e05de1b006b7cec5e6f28c30b138a61
Martynas <martynas@atomgraph.com>
2022-03-10 00:44:29 +0100

    Changed script interpreter line from "#!/bin/bash" to "#!/usr/bin/env bash" (should make them zsh compatible)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # check required bash version
--- scripts/webid-uri.sh
+++ scripts/webid-uri.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$#" -ne 1 ]; then
     echo "Usage:   $0" '$public_key_pem_file' >&2
