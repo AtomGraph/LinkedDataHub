a7ff48023c817b30adaf8bf9a31c96e337678594
Martynas <martynas@atomgraph.com>
2022-03-13 16:03:35 +0100

    Test that even members of the "owners" group cannot DELETE the owner or secretary WebID documents (400 Bad Request is returned)

--- http-tests/graph-store-protocol/DELETE-secretary-400.sh
+++ http-tests/graph-store-protocol/DELETE-secretary-400.sh
@@ -8,21 +8,23 @@ purge_backend_cache "$ADMIN_VARNISH_SERVICE"
 
 pushd . > /dev/null && cd "$SCRIPT_ROOT/admin/acl"
 
-# add agent to the writers
+# add agent to the owners
 
 ./add-agent-to-group.sh \
   -f "$OWNER_CERT_FILE" \
   -p "$OWNER_CERT_PWD" \
   --agent "$AGENT_URI" \
-  "${ADMIN_BASE_URL}acl/groups/writers/"
+  "${ADMIN_BASE_URL}acl/groups/owners/"
 
 popd > /dev/null
 
 # check that it's not possible to delete the root document
 
+secretary_doc=$(echo "$SECRETARY_URI" | cut -d '#' -f 1)
+
 curl -k -w "%{http_code}\n" -o /dev/null -s \
   -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
   -X DELETE \
   -H "Accept: application/n-triples" \
-  "$END_USER_BASE_URL" \
+  "$secretary_doc" \
 | grep -q "$STATUS_BAD_REQUEST"
