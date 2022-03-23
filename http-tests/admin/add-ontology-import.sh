c25dad8cae82ac4650bcf712d96cadc125933ba3
Martynas <martynas@atomgraph.com>
2022-03-23 23:07:27 +0100

    Use the ACL ontology in the owl:import test because it's not imported into the namespace ontology by default

--- http-tests/admin/add-ontology-import.sh
+++ http-tests/admin/add-ontology-import.sh
@@ -12,10 +12,12 @@ ontology_doc="${ADMIN_BASE_URL}model/ontologies/namespace/"
 
 # add ontology import
 
+import_uri="http://www.w3.org/ns/auth/acl#"
+
 ./add-ontology-import.sh \
   -f "$OWNER_CERT_FILE" \
   -p "$OWNER_CERT_PWD" \
-  --import "http://xmlns.com/foaf/0.1/" \
+  --import "$import_uri" \
   "$ontology_doc"
 
 # clear ontology from memory
@@ -32,4 +34,4 @@ popd > /dev/null
 curl -k -f -s -N \
   -H "Accept: application/n-triples" \
   "${END_USER_BASE_URL}ns" \
-| grep "<${END_USER_BASE_URL}ns#> <http://www.w3.org/2002/07/owl#imports> <http://xmlns.com/foaf/0.1/>" > /dev/null
+| grep "<${END_USER_BASE_URL}ns#> <http://www.w3.org/2002/07/owl#imports> <${import_uri}>" > /dev/null
