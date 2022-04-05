f1ef3d543f0bcb62cdf41d4a5b818bff92194394
Martynas <martynas@atomgraph.com>
2022-04-06 00:57:59 +0200

    Fixed the --ontology value in the add-ontology-import.sh test

--- http-tests/admin/add-ontology-import.sh
+++ http-tests/admin/add-ontology-import.sh
@@ -8,6 +8,8 @@ purge_backend_cache "$ADMIN_VARNISH_SERVICE"
 
 pushd . > /dev/null && cd "$SCRIPT_ROOT/admin"
 
+namespace_doc="${END_USER_BASE_URL}ns"
+namespace="${namespace_doc}#"
 ontology_doc="${ADMIN_BASE_URL}model/ontologies/namespace/"
 import_uri="http://www.w3.org/ns/auth/acl"
 
@@ -19,22 +21,22 @@ import_uri="http://www.w3.org/ns/auth/acl"
   --import "$import_uri" \
   "$ontology_doc"
 
-# clear ontology from memory
+# clear the namespace ontology from memory
 
 ./clear-ontology.sh \
   -f "$OWNER_CERT_FILE" \
   -p "$OWNER_CERT_PWD" \
   -b "$ADMIN_BASE_URL" \
-  --ontology "$import_uri"
+  --ontology "$namespace"
 
 popd > /dev/null
 
-# check that the import is present in the ontology
+# check that the import is present in the namespace ontology
 
 curl -k -f -s -N \
   -H "Accept: application/n-triples" \
-  "${END_USER_BASE_URL}ns" \
-| grep "<${END_USER_BASE_URL}ns#> <http://www.w3.org/2002/07/owl#imports> <${import_uri}>" > /dev/null
+  "$namespace_doc" \
+| grep "<${namespace}> <http://www.w3.org/2002/07/owl#imports> <${import_uri}>" > /dev/null
 
 # check that the imported ontology is present in the ontology model TO-DO: replace with an ASK query when #118 is fixed
 
@@ -43,5 +45,5 @@ curl -k -f -s \
   -E "$OWNER_CERT_FILE":"$OWNER_CERT_PWD" \
   -H 'Accept: application/sparql-results+xml' \
   --data-urlencode "query=SELECT * { <${import_uri}> ?p ?o }" \
-  "${END_USER_BASE_URL}ns" \
+  "$namespace_doc" \
 | grep '<literal>Basic Access Control ontology</literal>' > /dev/null
