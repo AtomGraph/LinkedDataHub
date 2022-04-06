2737818cc7151449bf16023938f1f87577ee9c51
Martynas <martynas@atomgraph.com>
2022-04-06 10:45:08 +0200

    Clear::post redirects back to Referer, if such request header is set

--- src/main/java/com/atomgraph/linkeddatahub/resource/admin/Clear.java
+++ src/main/java/com/atomgraph/linkeddatahub/resource/admin/Clear.java
@@ -29,7 +29,6 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
@@ -69,7 +68,6 @@ public class Clear
     {
         if (ontologyURI == null) throw new BadRequestException("Ontology URI not specified");
 
-//        String ontologyURI = clearURI.toString();
         EndUserApplication app = getApplication().as(AdminApplication.class).getEndUserApplication(); // we're assuming the current app is admin
         OntModelSpec ontModelSpec = new OntModelSpec(getSystem().getOntModelSpec(app));
         if (ontModelSpec.getDocumentManager().getFileManager().hasCachedModel(ontologyURI))
@@ -88,10 +86,8 @@ public class Clear
             ontModel.listImportedOntologyURIs(true).forEach((String importURI) -> addDocumentModel(ontModel.getDocumentManager(), importURI));
         }
         
-        ResponseBuilder builder = Response.ok();
-        if (referer != null) builder.location(referer);
-        
-        return builder.build();
+        if (referer != null) return Response.seeOther(referer).build();
+        else return Response.ok().build();
     }
     
     /**
