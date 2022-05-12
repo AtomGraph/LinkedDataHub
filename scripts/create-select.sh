90ff2834ef311c9a621cea039d08cc0ba0b45b70
Martynas <martynas@atomgraph.com>
2022-05-12 20:12:29 +0200

    Fixed ldh: prefix in create-select.sh

--- scripts/create-select.sh
+++ scripts/create-select.sh
@@ -114,6 +114,7 @@ args+=("-t")
 args+=("text/turtle") # content type
 args+=("${base}service")
 
+turtle+="@prefix ldh:	<https://w3id.org/atomgraph/linkeddatahub#> .\n"
 turtle+="@prefix dh:	<https://www.w3.org/ns/ldt/document-hierarchy#> .\n"
 turtle+="@prefix dct:	<http://purl.org/dc/terms/> .\n"
 turtle+="@prefix foaf:	<http://xmlns.com/foaf/0.1/> .\n"
@@ -128,7 +129,6 @@ turtle+="_:item foaf:primaryTopic _:query .\n"
 turtle+="_:item sioc:has_container <${container}> .\n"
 
 if [ -n "$service" ] ; then
-    turtle+="@prefix ldh:	<https://w3id.org/atomgraph/linkeddatahub#> .\n"
     turtle+="_:query ldh:service <${service}> .\n"
 fi
 if [ -n "$description" ] ; then
