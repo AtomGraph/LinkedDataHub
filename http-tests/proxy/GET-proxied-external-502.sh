0e0a20ec1d0f7ebd1f9b3085438f6aba665e5ba4
Martynas <martynas@atomgraph.com>
2022-05-20 00:23:05 +0200

    Test that LD proxy returns Bad Gateway exception if the external URL does not dereference

---
+++ http-tests/proxy/GET-proxied-external-502.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_backend_cache "$END_USER_VARNISH_SERVICE"
+purge_backend_cache "$ADMIN_VARNISH_SERVICE"
+
+pushd . > /dev/null && cd "$SCRIPT_ROOT/admin/acl"
+
+# add agent to the readers group to be able to write documents (might already be done by another test)
+
+./add-agent-to-group.sh \
+  -f "$OWNER_CERT_FILE" \
+  -p "$OWNER_CERT_PWD" \
+  --agent "$AGENT_URI" \
+  "${ADMIN_BASE_URL}acl/groups/readers/"
+
+popd > /dev/null
+
+# check for Bad Gateway exception if the external URL does not dereference
+
+curl -k -w "%{http_code}\n" -o /dev/null -s \
+  -G \
+  -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
+  --data-urlencode "uri=http://f1d2d4cf-90bb-4f5b-ae4b-921e584b6edd.org" \
+  "$END_USER_BASE_URL" \
+| grep -q "$STATUS_BAD_GATEWAY"
