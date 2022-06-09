674727b0106a76e5aa66592c50d78e5857578036
Martynas <martynas@atomgraph.com>
2022-06-09 21:55:35 +0200

    GSP PATCH method tests

---
+++ http-tests/graph-store-protocol/PATCH.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_backend_cache "$END_USER_VARNISH_SERVICE"
+purge_backend_cache "$ADMIN_VARNISH_SERVICE"
+
+pushd . > /dev/null && cd "$SCRIPT_ROOT/admin/acl"
+
+# add agent to the writers group
+
+./add-agent-to-group.sh \
+  -f "$OWNER_CERT_FILE" \
+  -p "$OWNER_CERT_PWD" \
+  --agent "$AGENT_URI" \
+  "${ADMIN_BASE_URL}acl/groups/writers/"
+
+popd > /dev/null
+
+# patch document
+
+curl -k -w "%{http_code}\n" -o /dev/null -f -s -G \
+  -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
+  -X PATCH \
+  -H "Content-Type: application/sparql-update" \
+  "$container" \
+   --data-binary @- <<EOF
+PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+INSERT
+{
+  <${END_USER_BASE_URL}> rdf:_2 <${END_USER_BASE_URL}#whateverest>
+}
+WHERE
+{}
+EOF
+| grep -q "$STATUS_OK"
+
+# check that the graph is changed
+
+curl -k -f -s -G \
+  -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
+  -H "Accept: application/n-triples" \
+"$END_USER_BASE_URL" \
+| grep "<${END_USER_BASE_URL}#whateverest>" > /dev/null
