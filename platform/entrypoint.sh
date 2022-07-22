27620608ee601919c97c0ec482372f0d945c4cf0
Martynas <martynas@atomgraph.com>
2022-07-23 00:18:54 +0300

    Create $OIDC_REFRESH_TOKENS parent folders

--- platform/entrypoint.sh
+++ platform/entrypoint.sh
@@ -620,6 +620,8 @@ fi
 # create empty properties file if it doesn't exist
 
 if [ -n "$OIDC_REFRESH_TOKENS" ] && [ ! -f "$OIDC_REFRESH_TOKENS" ]; then
+    mkdir -p "$(dirname "$OIDC_REFRESH_TOKENS")"
+
     touch "$OIDC_REFRESH_TOKENS"
 fi
 
