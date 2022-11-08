716e107b5fbb83f7e8c559a50e27639766342ca8
Martynas <martynas@atomgraph.com>
2022-11-08 23:25:41 +0100

    Restructured sparql-protocol tests Fixed $STATUS_METHOD_NOT_ALLOWED value

---
+++ http-tests/sparql-protocol/query/GET-ns.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_backend_cache "$END_USER_VARNISH_SERVICE"
+purge_backend_cache "$ADMIN_VARNISH_SERVICE"
+
+# SPARQL update on the <ns> endpoint should not be allowed
+
+(
+curl -k "%{http_code}\n" -f -s -G \
+  -H "Accept: application/n-triples" \
+  "${END_USER_BASE_URL}sparql" \
+  --data-urlencode "query=SELECT * { ?s ?p ?o }" \
+| grep -q "${STATUS_OK}"
