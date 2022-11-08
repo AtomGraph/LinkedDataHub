14ac3de3b8bfc6dde6b86870e76b5d3aebc73a81
Martynas <martynas@atomgraph.com>
2022-11-08 23:36:13 +0100

    Removed the -f option which made curl fail before writing error code

--- http-tests/sparql-protocol/update/POST-ns-405.sh
+++ http-tests/sparql-protocol/update/POST-ns-405.sh
@@ -9,7 +9,7 @@ purge_backend_cache "$ADMIN_VARNISH_SERVICE"
 # SPARQL update on the <ns> endpoint should not be allowed
 
 (
-curl -k -w "%{http_code}\n" -f -s \
+curl -k -w "%{http_code}\n" -s \
   -E "$OWNER_CERT_FILE":"$OWNER_CERT_PWD" \
   -H "Content-Type: application/sparql-update" \
   -H "Accept: application/n-triples" \
