db23da2f875cbbce164af453c76b53f90dcca855
Martynas <martynas@atomgraph.com>
2023-05-05 23:21:25 +0200

    Set $admin_endpoint_url in sitemap.rq.template

--- scripts/sitemap/generate-sitemap.sh
+++ scripts/sitemap/generate-sitemap.sh
@@ -1,6 +1,9 @@
 base="$1"
-endpoint="${1}sparql"
+endpoint="${base}sparql"
+export admin_endpoint_url="${base}admin/sparql"
 
-curl -k -G -H "Accept: application/sparql-results+xml" "$endpoint" --data-urlencode "query@../../platform/sitemap/sitemap.rq" -o results.xml
+envsubst < ../../platform/sitemap/sitemap.rq.template > sitemap.rq
+
+curl -k -G -H "Accept: application/sparql-results+xml" "$endpoint" --data-urlencode "query@sitemap.rq" -o results.xml
 
 docker run --rm -v "$PWD/../../platform/sitemap/sitemap.xsl":"/transform/sitemap.xsl" -v "$PWD/results.xml":"/transform/results.xml" atomgraph/saxon -s:/transform/results.xml -xsl:/transform/sitemap.xsl
