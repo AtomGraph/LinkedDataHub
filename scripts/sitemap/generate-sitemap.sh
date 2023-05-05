b9b3e8b92ed6f1e1e1414de30f87c58c07d0f4b8
Martynas <martynas@atomgraph.com>
2023-05-05 23:25:09 +0200

    Sitemap script uses exposed Fuseki ports

--- scripts/sitemap/generate-sitemap.sh
+++ scripts/sitemap/generate-sitemap.sh
@@ -1,9 +1,8 @@
-base="$1"
-endpoint="${base}sparql"
-export admin_endpoint_url="${base}admin/sparql"
+end_user_endpoint="http://localhost:3031/ds"
+export admin_endpoint_url="http://localhost:3030/ds"
 
 envsubst < ../../platform/sitemap/sitemap.rq.template > sitemap.rq
 
-curl -k -G -H "Accept: application/sparql-results+xml" "$endpoint" --data-urlencode "query@sitemap.rq" -o results.xml
+curl -k -G -H "Accept: application/sparql-results+xml" "$end_user_endpoint" --data-urlencode "query@sitemap.rq" -o results.xml
 
 docker run --rm -v "$PWD/../../platform/sitemap/sitemap.xsl":"/transform/sitemap.xsl" -v "$PWD/results.xml":"/transform/results.xml" atomgraph/saxon -s:/transform/results.xml -xsl:/transform/sitemap.xsl
