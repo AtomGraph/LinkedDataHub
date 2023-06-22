b9376c0e7053478fa18f6353860b275823825a23
Martynas <martynas@atomgraph.com>
2023-06-22 11:08:28 +0200

    Replaced `BadRequestException` with `NotAllowedException`

--- src/main/java/com/atomgraph/linkeddatahub/server/model/impl/ProxyResourceBase.java
+++ src/main/java/com/atomgraph/linkeddatahub/server/model/impl/ProxyResourceBase.java
@@ -34,7 +34,7 @@ import java.util.Locale;
 import java.util.Optional;
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.NotAllowedException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
@@ -202,7 +202,7 @@ public class ProxyResourceBase extends com.atomgraph.client.model.impl.ProxyReso
             return getResponse(getDataManager().loadModel(target.getUri().toString()));
         }
 
-        if (!getSystem().isEnableLinkedDataProxy()) throw new BadRequestException("Linked Data proxy not enabled");
+        if (!getSystem().isEnableLinkedDataProxy()) throw new NotAllowedException("Linked Data proxy not enabled");
 
         return super.get(target, builder);
     }
@@ -217,7 +217,7 @@ public class ProxyResourceBase extends com.atomgraph.client.model.impl.ProxyReso
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response postMultipart(FormDataMultiPart multiPart)
     {
-        if (!getSystem().isEnableLinkedDataProxy()) throw new BadRequestException("Linked Data proxy not enabled");
+        if (!getSystem().isEnableLinkedDataProxy()) throw new NotAllowedException("Linked Data proxy not enabled");
         if (getWebTarget() == null) throw new NotFoundException("Resource URI not supplied"); // cannot throw Exception in constructor: https://github.com/eclipse-ee4j/jersey/issues/4436
         
         try (Response cr = getWebTarget().request().
@@ -239,7 +239,7 @@ public class ProxyResourceBase extends com.atomgraph.client.model.impl.ProxyReso
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response putMultipart(FormDataMultiPart multiPart)
     {
-        if (!getSystem().isEnableLinkedDataProxy()) throw new BadRequestException("Linked Data proxy not enabled");
+        if (!getSystem().isEnableLinkedDataProxy()) throw new NotAllowedException("Linked Data proxy not enabled");
         if (getWebTarget() == null) throw new NotFoundException("Resource URI not supplied"); // cannot throw Exception in constructor: https://github.com/eclipse-ee4j/jersey/issues/4436
         
         try (Response cr = getWebTarget().request().
