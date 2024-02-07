27e3b438299e69e95206cb07eb2915eccc3b08cd
Martynas <martynas@atomgraph.com>
2024-02-07 23:09:47 +0100

    Evaluate request preconditions in `Item::patch`

--- src/main/java/com/atomgraph/linkeddatahub/resource/graph/Item.java
+++ src/main/java/com/atomgraph/linkeddatahub/resource/graph/Item.java
@@ -51,9 +51,11 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.EntityTag;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Request;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.SecurityContext;
 import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.MessageBodyReader;
@@ -240,8 +242,12 @@ public class Item extends GraphStoreImpl
     {
         if (updateRequest == null) throw new BadRequestException("SPARQL update not specified");
 
-        final boolean existingGraph = getDatasetAccessor().containsModel(getURI().toString());
-        if (!existingGraph) throw new NotFoundException("Named graph with URI <" + getURI() + "> not found");
+        final Model existingGraph = getDatasetAccessor().getModel(getURI().toString());
+        if (existingGraph == null) throw new NotFoundException("Named graph with URI <" + getURI() + "> not found");
+        
+        EntityTag entityTag = getEntityTag(existingGraph);
+        ResponseBuilder rb = getRequest().evaluatePreconditions(entityTag);
+        if (rb != null) return rb.build(); // preconditions not met
 
         updateRequest.getOperations().forEach(update ->
         {
