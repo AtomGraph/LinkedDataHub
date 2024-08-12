54c749a7821cfbbc0d5a3ffc126723d0088c08ad
Martynas <martynas@atomgraph.com>
2024-08-12 23:46:10 +0200

    Added missing `foaf:` NS

--- scripts/admin/acl/make-public.sh
+++ scripts/admin/acl/make-public.sh
@@ -75,10 +75,11 @@ curl -X PATCH \
      --data-binary @- <<EOF
 BASE <${base}admin/>
 
-PREFIX  acl:  <http://www.w3.org/ns/auth/acl#>
+PREFIX  acl: <http://www.w3.org/ns/auth/acl#>
 PREFIX  def: <https://w3id.org/atomgraph/linkeddatahub/default#>
 PREFIX  dh:  <https://www.w3.org/ns/ldt/document-hierarchy#>
 PREFIX  nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+PREFIX  foaf: <http://xmlns.com/foaf/0.1/>
 
 INSERT
 {
