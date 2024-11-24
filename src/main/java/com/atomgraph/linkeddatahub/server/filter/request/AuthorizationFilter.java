0b3db349e94c89e98057f7825ecff829e7e8d90e
Martynas <martynas@atomgraph.com>
2024-11-25 00:12:22 +0100

    Further optimization of the authorization query

--- src/main/java/com/atomgraph/linkeddatahub/server/filter/request/AuthorizationFilter.java
+++ src/main/java/com/atomgraph/linkeddatahub/server/filter/request/AuthorizationFilter.java
@@ -158,7 +158,7 @@ public class AuthorizationFilter implements ContainerRequestFilter
     public QuerySolutionMap getAuthorizationParams(Resource absolutePath, Resource agent, Resource accessMode)
     {
         QuerySolutionMap qsm = new QuerySolutionMap();
-        qsm.add(SPIN.THIS_VAR_NAME, absolutePath);
+        qsm.add("thisValue", absolutePath); // ?this is now assigned using VALUES
         qsm.add("Mode", accessMode);
         qsm.add(LDT.Ontology.getLocalName(), getApplication().getOntology());
         qsm.add(LDT.base.getLocalName(), getApplication().getBase());
