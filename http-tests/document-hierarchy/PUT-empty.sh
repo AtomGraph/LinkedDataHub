6d93326d8853036644a3465fe6871b6abeee8371
Martynas Jusevičius <martynas@atomgraph.com>
2025-04-01 00:00:01 +0200

    New HTTP test for `PUT` with empty request body
    Increased burst limit for static files

---
+++ http-tests/document-hierarchy/PUT-empty.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+initialize_dataset "$END_USER_BASE_URL" "$TMP_END_USER_DATASET" "$END_USER_ENDPOINT_URL"
+initialize_dataset "$ADMIN_BASE_URL" "$TMP_ADMIN_DATASET" "$ADMIN_ENDPOINT_URL"
+purge_cache "$END_USER_VARNISH_SERVICE"
+purge_cache "$ADMIN_VARNISH_SERVICE"
+purge_cache "$FRONTEND_VARNISH_SERVICE"
+
+# add agent to the writers
+
+add-agent-to-group.sh \
+  -f "$OWNER_CERT_FILE" \
+  -p "$OWNER_CERT_PWD" \
+  --agent "$AGENT_URI" \
+  "${ADMIN_BASE_URL}acl/groups/writers/"
+
+# check that graph without parent is forbidden
+
+(
+curl -k -w "%{http_code}\n" -o /dev/null -s \
+  -X PUT \
+  -E "$AGENT_CERT_FILE":"$AGENT_CERT_PWD" \
+  -H "Content-Type: application/n-triples" \
+   --data-binary @- \
+  "${END_USER_BASE_URL}non-existing/" <<EOF
+EOF
+) \
+| grep -q "$STATUS_OK"
