33b9c0bb3dfce8609383dfd7eedcd38b00e38c01
Martynas Jusevičius <martynas@atomgraph.com>
2025-12-26 13:51:18 +0100

    Refactored `XSLTMasterUpdater`

--- src/main/java/com/atomgraph/linkeddatahub/server/util/XsltMasterUpdater.java
+++ src/main/java/com/atomgraph/linkeddatahub/server/util/XsltMasterUpdater.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-
 import jakarta.servlet.ServletContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
